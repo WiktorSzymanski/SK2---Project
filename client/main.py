64b31a07d01898329e129baddfc826fa5c08211b
BartoszDolata <bartosz.dolata@student.put.poznan.pl>
2023-01-24 12:41:05 +0100

    modify user

--- client/main.py
+++ client/main.py
@@ -4,27 +4,34 @@ import tkinter as tk
 from tkinter import *
 from tkinter import simpledialog, scrolledtext
 
-HOST = '127.0.0.1'
+HOST = '192.168.55.105'
 PORT = 1234
 
 
 class Client:
   def __init__(self, host, port):
-    # self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # self.sock.connect((host, port))
+
     msg = tk.Tk()
     msg.withdraw()
     self.nickname = simpledialog.askstring("Nickname", "Please enter your nickname", parent=msg)
-
+    self.current_reciver = None
+    self.friends_array = ['Jan', 'Dzban', "Wan", 'Pan', 'Chłam']
+    # self.host = simpledialog.askstring("Server address", "Please enter server address", parent=msg)
+    try:
+      self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+      self.sock.connect((host, port))
+    except:
+      print("Some issue with connection")
     self.gui_done = False
 
     self.running = True
-
-    gui_thread = threading.Thread(target=self.gui_loop())
-    # recive_thread = threading.Thread(targer = recive)
+    gui_thread = threading.Thread(target=self.gui_loop)
+    recive_thread = threading.Thread(target=self.recive)
 
     gui_thread.start()
-    # recive_thread.start()
+    recive_thread.start()
+
+
 
   def gui_loop(self):
     self.win = tk.Tk()
@@ -39,35 +46,45 @@ class Client:
     self.frameright_msg = Frame(self.frameright)
     self.frameright_input = Frame(self.frameright)
 
-
-    #main label
+    # main label
     self.win_label = tk.Label(self.frametop, text="Name: {}".format(self.nickname))
     self.win_label.config(font=("Arial", 12))
-    self.win_label.pack(side = RIGHT, padx=10, pady=10)
+    self.win_label.pack(side=RIGHT, padx=10, pady=10)
     self.frametop.pack(side=TOP, fill=BOTH, expand=1, padx=10, pady=10)
 
-    #nav pane
-    self.users_area = Listbox(self.framenav,selectbackground="lightblue")
-    for i in range(10):
-      self.users_area.insert(i,"User {}".format(str(i)))
+    def callback(event):
+      selection = event.widget.curselection()
+      if selection:
+        index = selection[0]
+        data = event.widget.get(index)
+        print("You selected ", data)
+        self.current_reciver = data
+
+
+    # nav pane
+    self.users_area = Listbox(self.framenav, selectbackground="lightblue")
+    self.users_area.bind("<<ListboxSelect>>", callback)
+    for i in range(len(self.friends_array)):
+      self.users_area.insert(i, (self.friends_array[i]))
 
     self.users_area.pack(side=LEFT, fill=BOTH, expand=1, padx=5, pady=5)
 
     self.framenav.pack(side=TOP, fill=BOTH, expand=1, padx=5, pady=5)
 
-    #chat pane
+    # chat pane
+
     self.text_area = scrolledtext.ScrolledText(self.frameright)
-    self.text_area.pack(side=TOP, fill=BOTH, expand=1,padx=5, pady=5)
+    self.text_area.pack(side=TOP, fill=BOTH, expand=1, padx=5, pady=5)
     self.text_area.config(state='disable')
 
     self.frameright.pack(side=TOP, fill=BOTH, expand=1, padx=5, pady=5)
-    #input message pane
+    # input message pane
     self.sending_frame = tk.Frame(self.frameright)
 
     self.input_msg = tk.Text(self.sending_frame, height=2)
     self.input_msg.pack(side=LEFT, fill=BOTH, expand=1, padx=5, pady=5)
-
-    self.input_btn = tk.Button(self.sending_frame, text=">", command=self.test)
+    self.win.bind('<Return>', self.write)
+    self.input_btn = tk.Button(self.sending_frame, text=">", command=self.write)
     self.input_btn.config(font=("Arial", 16))
     self.input_btn.pack(side=RIGHT, fill=BOTH, expand=1, padx=5, pady=5)
 
@@ -87,18 +104,39 @@ class Client:
   def stop(self):
     self.running = False
     self.win.destroy()
-    # self.sock.close()
+    message = "wyjeb mnie"
+    self.sock.send(message.encode("utf-8"))
+    self.sock.close()
     print("User exit system")
     exit(0)
 
-  # def recive(self):
-  #   pass
-  #
-  # def send(self):
-  #   pass
+
+
   def test(self):
     print("wyslano od {}".format(self.nickname))
+
+  def write(self):
+    # message = str(self.nickname)+'\n'+str(self.current_reciver)+'\n'+str(self.input_msg.get('1.0', 'end'))
+    message = str(self.nickname)+':'+str(self.current_reciver)+':'+str(self.input_msg.get('1.0', 'end'))
+    self.sock.send(message.encode("utf-8"))
+    print(message)
     self.input_msg.delete('1.0', 'end')
+
+  def recive(self):
+    while self.running:
+      try:
+        message = self.sock.recv(512).decode('utf-8')
+        if self.gui_done:
+          self.text_area.config(state="normal")
+          self.text_area.insert('end', message)
+          self.text_area.yview('end')
+          self.text_area.config(state='disabled')
+      except ConnectionAbortedError:
+        break
+      except:
+        print("error")
+        self.sock.close()
+        break
 
 
 client = Client(HOST, PORT)
